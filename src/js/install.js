92958b0c7ffe7199d93502aaf83924e14d754390
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-06-01 11:50:05 -0500

    webpack.config/srcpsw

--- src/js/install.js
+++ src/js/install.js
@@ -1,33 +1,32 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Event handler for 'beforeinstallprompt'
 window.addEventListener('beforeinstallprompt', (event) => {
-    
+
+    // Store the triggered events
     window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
+  });
 
-});
-
-// Implements 'click' event listener for 'butInstall' 
 butInstall.addEventListener('click', async () => {
-   
-    const promptEvent = window.deferredPrompt;
-
-    if (!promptEvent) {
-     return;
-    }
   
-    promptEvent.prompt();
-    
-    window.deferredPrompt = null;
-    
-    butInstall.classList.toggle('hidden', true);
+  const promptEvent = window.deferredPrompt;
 
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
-// Event handler for 'appinstalled'
 window.addEventListener('appinstalled', (event) => {
-    
-    window.deferredPrompt = null;
-    
-});
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
